b94a6d1ed13889888a4dc5a194e7bd228a3ded2b
Antonio MM <antoniomm80@gmail.com>
2025-04-04 21:17:17 +0200

    fix: Paths normalization

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import {BrowserRouter} from "react-router";
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <BrowserRouter basename="/chef-app">
+        <BrowserRouter basename="/recipes-app">
             <App/>
         </BrowserRouter>
     </StrictMode>,
